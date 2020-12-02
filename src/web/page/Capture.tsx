afff7e8f28b757f66dd93e87be41af3076537618
himself65 <himself65@outlook.com>
2020-12-02 16:58:00 +0800

    fix(web): global css in CapturePage

--- src/web/page/Capture.tsx
+++ src/web/page/Capture.tsx
@@ -1,16 +1,24 @@
-import '../css/index.less'
-
 import { Paper } from '@material-ui/core'
+import OpenWithIcon from '@material-ui/icons/OpenWith'
 import { makeStyles } from '@material-ui/styles'
 import React from 'react'
 
 const useStyles = makeStyles({
   root: {
     width: '100%',
-    height: '100%'
+    height: '100%',
+    background: 'rgba(0, 0, 0, 0.2)'
   },
   dragArea: {
     '-webkit-app-region': 'drag'
+  },
+  // overwrite the global css
+  '@global': {
+    'html, body, #app': {
+      width: '100%',
+      height: '100%',
+      background: 'rgba(0, 0, 0, 0.2)'
+    }
   }
 })
 
@@ -18,8 +26,8 @@ export const CapturePage: React.FC = () => {
   const classes = useStyles()
   // todo(unfinished)
   return (
-    <Paper className={`${classes.root} ${classes.dragArea}`}>
-      Capture Page
+    <Paper className={classes.root}>
+      <OpenWithIcon className={classes.dragArea}/>
     </Paper>
   )
 }
